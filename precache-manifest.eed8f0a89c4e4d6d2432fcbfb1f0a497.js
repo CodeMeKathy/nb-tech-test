e4dc60900ca1f650a0ccc8678dfa08049a4711f4
Kathy * <Kathy@KathyLambert.me>
2019-05-10 17:17:51 -0400

    Updates

---
+++ precache-manifest.eed8f0a89c4e4d6d2432fcbfb1f0a497.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51209f48bbf65b9a7b792407921ba2ba",
+    "url": "/nb-tech-test/index.html"
+  },
+  {
+    "revision": "cba77c66d0370cf8b6cf",
+    "url": "/nb-tech-test/static/css/main.082d73a9.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/nb-tech-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "cba77c66d0370cf8b6cf",
+    "url": "/nb-tech-test/static/js/main.0e3e9e26.chunk.js"
+  },
+  {
+    "revision": "3b6f5d4292e876d5c7f4",
+    "url": "/nb-tech-test/static/js/runtime~main.1e9dc664.js"
+  },
+  {
+    "revision": "06c32e2e829f0124d141544f09d36594",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_bd-webfont.06c32e2e.woff"
+  },
+  {
+    "revision": "3fcc20fe5d7b4ec1f6b36751ae6ace4f",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_bd-webfont.3fcc20fe.woff2"
+  },
+  {
+    "revision": "376d0bde971e716ac4fdb79b76721c4b",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_bdit-webfont.376d0bde.woff"
+  },
+  {
+    "revision": "7a1f96a17afd3bb45343b3d5483b4044",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_bdit-webfont.7a1f96a1.woff2"
+  },
+  {
+    "revision": "895c2162d7fd0ed86b9b369a6d3e1cee",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_hair-webfont.895c2162.woff2"
+  },
+  {
+    "revision": "db889560b41d007610c6e7243c4f2aaf",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_hair-webfont.db889560.woff"
+  },
+  {
+    "revision": "3e413a33992e9082ee6bff9df1022a7b",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_hairit-webfont.3e413a33.woff2"
+  },
+  {
+    "revision": "a79b9a10bcdb588718a3f20ef057c511",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_hairit-webfont.a79b9a10.woff"
+  },
+  {
+    "revision": "3f3d4aa26d6560405765a628e8fec98b",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_it-webfont.3f3d4aa2.woff2"
+  },
+  {
+    "revision": "588e59b07950c6976cc09408c3bd3637",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_it-webfont.588e59b0.woff"
+  },
+  {
+    "revision": "1a95bc313ced747644d3b3b4c21076a9",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_lt-webfont.1a95bc31.woff2"
+  },
+  {
+    "revision": "adb9f5793f4f81e5bad5a131a6f7479d",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_lt-webfont.adb9f579.woff"
+  },
+  {
+    "revision": "7df746f83b0d50c2520cd27605a26282",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_ltit-webfont.7df746f8.woff"
+  },
+  {
+    "revision": "d3641219bfc49effcfcd6ea6d2a8058e",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_ltit-webfont.d3641219.woff2"
+  },
+  {
+    "revision": "03135637af795606e0566544f3c0e2f1",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_md-webfont.03135637.woff"
+  },
+  {
+    "revision": "485a9989486f131e2345d6776d20a33e",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_md-webfont.485a9989.woff2"
+  },
+  {
+    "revision": "070d82bd6a0ea258e701672b15432d2f",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_mdit-webfont.070d82bd.woff"
+  },
+  {
+    "revision": "7c46bbeb63110e5c121ee3edaa5964af",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_mdit-webfont.7c46bbeb.woff2"
+  },
+  {
+    "revision": "a1d70ef23493a7adb6ef035d0c0b8bcf",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_rg-webfont.a1d70ef2.woff2"
+  },
+  {
+    "revision": "c6d6679a2ef89a7a418cf744e643fb48",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_rg-webfont.c6d6679a.woff"
+  },
+  {
+    "revision": "4a9b16a47d8d2a62bfbebf229ef36d93",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_th-webfont.4a9b16a4.woff2"
+  },
+  {
+    "revision": "9567fdc62d851b5a78750e3983837c52",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_th-webfont.9567fdc6.woff"
+  },
+  {
+    "revision": "8226b1aedf84336166747d3cfd5930e1",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_thit-webfont.8226b1ae.woff"
+  },
+  {
+    "revision": "b7946aaf28ca4fa1fa0ed7bbbd8832f9",
+    "url": "/nb-tech-test/static/media/aktivgrotesk_thit-webfont.b7946aaf.woff2"
+  },
+  {
+    "revision": "057c2d0dd301c661bef8583aa870c326",
+    "url": "/nb-tech-test/static/media/logo.057c2d0d.svg"
+  },
+  {
+    "revision": "c98371896a9e67f50a29fc165db36789",
+    "url": "/nb-tech-test/static/media/merriweather-black-webfont.c9837189.woff"
+  },
+  {
+    "revision": "cd349085a18b9a90efa3ecc6413b5a8a",
+    "url": "/nb-tech-test/static/media/merriweather-black-webfont.cd349085.woff2"
+  },
+  {
+    "revision": "0ce94c1de63f1599e6db120945ebad8a",
+    "url": "/nb-tech-test/static/media/merriweather-blackitalic-webfont.0ce94c1d.woff"
+  },
+  {
+    "revision": "17efa817533e298fbedcf1dc46bb2232",
+    "url": "/nb-tech-test/static/media/merriweather-blackitalic-webfont.17efa817.woff2"
+  },
+  {
+    "revision": "77fb955a7ea4d1338212fc096991d698",
+    "url": "/nb-tech-test/static/media/merriweather-bold-webfont.77fb955a.woff"
+  },
+  {
+    "revision": "b07a63c2b5c12db392fbf6616aa2a4a8",
+    "url": "/nb-tech-test/static/media/merriweather-bold-webfont.b07a63c2.woff2"
+  },
+  {
+    "revision": "6146cd1fcf0932c85cf24e4e58ed04f2",
+    "url": "/nb-tech-test/static/media/merriweather-bolditalic-webfont.6146cd1f.woff2"
+  },
+  {
+    "revision": "e159330a44a35a3c8527f1284b7b4829",
+    "url": "/nb-tech-test/static/media/merriweather-bolditalic-webfont.e159330a.woff"
+  },
+  {
+    "revision": "e2db3ec635ee3ea9ce366fd421a236e4",
+    "url": "/nb-tech-test/static/media/merriweather-italic-webfont.e2db3ec6.woff"
+  },
+  {
+    "revision": "f4c061edb8382fa76882daf3bb231252",
+    "url": "/nb-tech-test/static/media/merriweather-italic-webfont.f4c061ed.woff2"
+  },
+  {
+    "revision": "6caea0247db4f71450c18a2d452c99e9",
+    "url": "/nb-tech-test/static/media/merriweather-light-webfont.6caea024.woff2"
+  },
+  {
+    "revision": "cf6f3a5a26d671f27745388e7c7baf0d",
+    "url": "/nb-tech-test/static/media/merriweather-light-webfont.cf6f3a5a.woff"
+  },
+  {
+    "revision": "6aec759d8b358bc3dcbadf39348474a1",
+    "url": "/nb-tech-test/static/media/merriweather-lightitalic-webfont.6aec759d.woff2"
+  },
+  {
+    "revision": "c66c1d7e4ee85cebbda37a393359cb3d",
+    "url": "/nb-tech-test/static/media/merriweather-lightitalic-webfont.c66c1d7e.woff"
+  },
+  {
+    "revision": "d202568835d2beaf4dd47411605dd69e",
+    "url": "/nb-tech-test/static/media/merriweather-regular-webfont.d2025688.woff"
+  },
+  {
+    "revision": "d5d8e0394ea2dd0201b61dad4f92274f",
+    "url": "/nb-tech-test/static/media/merriweather-regular-webfont.d5d8e039.woff2"
+  }
+]);
