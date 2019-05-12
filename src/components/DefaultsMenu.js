2f5a82f9e37d3e5efe2caebe0f3df460ecb02992
Kathy * <Kathy@KathyLambert.me>
2019-05-12 11:34:05 -0400

    Added the defaults navbar with tabs to the DefaultsMenu.js. Imported the DefaultsMenu component into App.js

---
+++ src/components/DefaultsMenu.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../css/DefaultsMenu.css'
+
+export default class DefaultsMenu extends React.Component {
+  render() {
+    return (
+      <div className='defaults-menu-wrapper'>
+        <nav className='defaults-navbar' >
+            <ul className="nav nav-tabs defaults-tabs">
+              <li className="nav-item">
+                <a className="nav-link defaults-link" href="#">Basics</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link defaults-link" href="#">Custom fields</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link defaults-link active" href="#">Social capital</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link defaults-link" href="#">Finacne types</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link defaults-link" href="#">Dashboards</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link defaults-link" href="#">Permission sets</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link defaults-link" href="#">Unsubscribe</a>
+              </li>
+            </ul>
+          </nav>
+      </div>
+    )
+  }
+}
