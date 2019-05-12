80373d85a0d11f9fc14d953d856b172639573693
Kathy * <Kathy@KathyLambert.me>
2019-05-12 11:34:05 -0400

    Added the defaults navbar with tabs to the DefaultsMenu.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import SettingsHeader from './components/SettingsHeader.js'
+import DefaultsMenu from './components/DefaultsMenu'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <SettingsHeader />
+      <DefaultsMenu />
     </div>
   );
 }
