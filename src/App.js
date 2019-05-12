29d87cffe758035eacbe7c2e1224a4be5d207f37
Kathy * <Kathy@KathyLambert.me>
2019-05-12 10:43:29 -0400

    Added SearchBar.js and SearchBar.css.  Modified the SettingsHeader.js navbar and imported in the SearchBar component.  Added font awesome imports for SearchBar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 import SettingsHeader from './components/SettingsHeader.js'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSearch)
+
 function App() {
   return (
     <div className="App">
--- src/components/SettingsHeader.js
+++ src/components/SettingsHeader.js
@@ -2,6 +2,8 @@ import React from 'react'
 import '../App.css'
 import '../css/SettingsHeader.css'
 
+import SearchBar from '../components/SearchBar'
+
 export default class SettingsHeader extends React.Component {
   render() {
     return (
@@ -9,38 +11,40 @@ export default class SettingsHeader extends React.Component {
         <div className='settings-wrapper'>
           <img id='avatar' src='https://github.com/CodeMeKathy/nb-tech-test/blob/master/public/images/Avatar.png?raw=true' alt="avatar"/>
           <h1 className='settings-title'>Settings</h1>
-          {/* <Searchbar /> */}
+
+          <SearchBar />
+
           <nav className='setting-navbar' >
             <ul className="nav nav-tabs settings-tabs">
               <li className="nav-item">
-                <a className="nav-link" href="#">Your account</a>
+                <a className="nav-link settings-link" href="#">Your account</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <a className="nav-link settings-link" href="#">Contact</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">Defaults</a>
+                <a className="nav-link settings-link active" href="#">Defaults</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Political</a>
+                <a className="nav-link settings-link" href="#">Political</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Domains</a>
+                <a className="nav-link settings-link" href="#">Domains</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Payment processors</a>
+                <a className="nav-link settings-link" href="#">Payment processors</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Apps</a>
+                <a className="nav-link settings-link" href="#">Apps</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Developer</a>
+                <a className="nav-link settings-link" href="#">Developer</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Database</a>
+                <a className="nav-link settings-link" href="#">Database</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Privacy</a>
+                <a className="nav-link settings-link" href="#">Privacy</a>
               </li>
             </ul>
           </nav>
