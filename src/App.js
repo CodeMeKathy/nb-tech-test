98924b9c37885824ab78d9838c599ad0cebc46b4
Kathy * <Kathy@KathyLambert.me>
2019-05-12 23:22:39 -0400

    Imported the CustomFieldsMenu component into App.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 import SettingsHeader from './components/SettingsHeader.js'
 import DefaultsMenu from './components/DefaultsMenu'
+import CustomFieldsMenu from './components/CustomFieldsMenu'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -14,6 +15,8 @@ function App() {
     <div className="App">
       <SettingsHeader />
       <DefaultsMenu />
+      <CustomFieldsMenu />
+
     </div>
   );
 }
